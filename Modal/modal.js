2df28bd6d19b439df41f76529cce1279e921198b
Manish <www.manish1997@gmail.com>
2022-02-17 18:13:36 +0530

    refactor: renamed button id and class

--- Modal/modal.js
+++ Modal/modal.js
@@ -1,9 +1,12 @@
 const modalBtn = document.querySelector("#display-modal");
-const xBtn = document.querySelector("#x-btn");
+const closeBtn = document.querySelector("#close-btn");
 const modalContainer = document.querySelector(".modal-container");
 
-xBtn.addEventListener("click", () => (modalContainer.style.display = "none"));
+closeBtn.addEventListener(
+	"click",
+	() => (modalContainer.style.display = "none")
+);
 modalBtn.addEventListener(
 	"click",
-	() => (modalContainer.style.display = "flex")
+	() => (modalContainer.style.display = "grid")
 );
